e557c1a4a796e99440e8bb05fd7a2a9315e9ae1a
Darren Pruitt <darren.pruitt@gmail.com>
2021-09-30 15:24:42 -0500

    Adding another neopixel project

---
+++ PlatformIO/NeoPixels/NeoPixel4x8 02/src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6     // Pin for data
+#define LEDS 32   // How many LEDs?
+#define ROWS 4    // count LEDs downward
+#define COLUMNS 8 //Count your LEDS to the right
+#define SPEED 100 //faster the lower the number
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+uint16_t neoPIxelIndex[4][8] =
+    {
+        {0, 1, 2, 3, 4, 5, 6, 7},
+        {8, 9, 10, 11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20, 21, 22, 23},
+        {24, 25, 26, 27, 28, 29, 30, 31}
+    };
+
+#include <Arduino.h>
+
+void setup()
+{
+  //Serial.begin(57600);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop()
+{
+  uint32_t c0 = strip.Color(4, 0, 0);
+
+  strip.setPixelColor(neoPIxelIndex[1][3], c0);
+  strip.show();
+  delay(SPEED);
+
+}
